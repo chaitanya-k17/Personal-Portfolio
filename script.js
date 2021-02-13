482e57bc576f89d5c77e3f0647c3ea15aecfb062
Chaitanya <chaitanyakhiratkar17@gmail.com>
2021-02-14 03:06:03 +0530

    docs
    Wellcome

---
+++ script.js
@@ -0,0 +1,10 @@
+var i=0;
+
+function sliding(argument) {
+	var img=document.getElementById("myImages");
+	img.src="images/pic"+ (i%2) +".jpg";
+	var img1=document.getElementById("myImages1");
+	img1.src="images/pics"+ (i%2) +".jpeg";
+	i++;
+}
+setInterval(function(){ sliding();},1500);
