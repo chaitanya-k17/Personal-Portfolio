f1d7d84858796b1a0dfbae9624a92ff4bc2e989f
Chaitanya <chaitanyakhiratkar17@gmail.com>
2021-02-17 21:02:32 +0530

    Wellcome

--- profileJS.js
+++ profileJS.js
@@ -3,8 +3,11 @@ var cllg="Third Year Chemical Engineering<br>IIT KHARAGPUR <br>";
 var i=0,j=0;
 
 function starting() {
-	if(i==20){
+	if(i>=19 && i<23){
 		document.getElementById("i-name").innerHTML=name;
+		i++;
+	}
+	else if(i==23){
 		document.getElementById("i-clg").innerHTML=cllg;
 		console.log("1");
 		clearInterval(Naming);
@@ -24,7 +27,7 @@ function sliding(x) {
 	if(x==0){
 		j++;
 	}
-	if(j==4){
+	if(j>3){
 		j=0;
 	}
 	if(j<0){
@@ -35,11 +38,48 @@ function sliding(x) {
 	
 }
 
+var dropdown=false;
+
+function loading() {
+	console.log("00099999888888");
+	if(!dropdown){
+		document.getElementById("main-page").setAttribute("style","visibility: visible;");
+		document.getElementById("page").setAttribute("style","overflow-y:scroll;");
+		document.getElementById("p-header").setAttribute("style","visibility: visible;");
+		document.getElementById("scroller").setAttribute("href","#p");
+		document.getElementById("i").setAttribute("style","min-height:500px;");
+		document.getElementById("dd-img").setAttribute("src","images/dropup.jpg");
+		dropdown=true;
+	}
+	else{
+		document.getElementById("main-page").setAttribute("style","visiblity:hidden;");
+		document.getElementById("page").setAttribute("style","overflow-y:hidden;");
+		document.getElementById("p-header").setAttribute("style","visibility: hidden;");
+		document.getElementById("scroller").setAttribute("href","#");
+		document.getElementById("i").setAttribute("style","min-height:1200px;");
+		document.getElementById("dd-img").setAttribute("src","images/dropdown.jpg");
+		dropdown=false;
+	}
+}
+
 
 function Checking() {
 	var nav=document.getElementById("nav-p");
 }
+function Messaging() {
+	var x=document.getElementsByClassName("input");
+	var y=document.getElementsByClassName("textarea");
+	if(x[0]==""){
+		alert("Enter e-mail id");
+	}
+	else if(x[1]==""){
+		alert("Enter Message");
+	}
+	else{
+		alert("Your Message has been send");
+	}
+}
 var Imaging=setInterval(function(){ sliding(0);},5000);
-var Naming=setInterval(function(){ starting();},400);
+var Naming=setInterval(function(){ starting();},200);
 
 
