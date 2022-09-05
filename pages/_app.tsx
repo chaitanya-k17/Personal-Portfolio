ec626712efd7352339d051a6f25f89503764cd62
Chaitaya Khiratkar <chaitanya.iitkgp@gmail.com>
2022-09-06 04:55:31 +0530

    Patched the Build issues

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,13 +19,13 @@ function MyApp({ Component, pageProps }: AppProps) {
         {/*Other Addtional Data  */}
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="google-site-verification" content="TtFDsGL1rsGC-ETYdYFFFD0MJXiBi1Tl1l_lTBP3mNk" />
+        <meta httpEquiv="REFRESH" content="0;url=https://chaitanyak.info"/>
         {/* Adding up the Meta Tags For Better SEO Optimizations */}
         <meta property="og:title" content="Chaitanya Khiratkar" />
         <meta property="og:url" content="https://chaitanya-khiratkar.web.app" />
-        <meta property="og:description" content="I am Chaitanya Khiratkar, a third-year undergraduate student at the Indian Institute of Technology, Kharagpur, one of the top universities in India. I am currently pursuing a major in Chemical Engineering ( B.Tech). During my course of study, I have developed a keen interest in Software Development." />
+        <meta property="og:description" content="Heyy, I am Chaitanya Khiratkar, a Final-year undergraduate student at the Indian Institute of Technology, Kharagpur, one of the top universities in India" />
         <meta property="og:type" content="profile" />
         <meta property="og:image" content="https://chaitanya-khiratkar.web.app/static/media/profile.e890939f.jpeg" />
-        <meta http-equiv="REFRESH" content="0;url=https://chaitanyak.info"/>
 
         
 
