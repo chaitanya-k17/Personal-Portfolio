d337d6a0840a9ed99a2b4babb2f0753ed174ebec
Chaitaya Khiratkar <chaitanya.iitkgp@gmail.com>
2022-09-06 05:01:08 +0530

    Updates HTTPS req redirect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,7 +19,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         {/*Other Addtional Data  */}
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="google-site-verification" content="TtFDsGL1rsGC-ETYdYFFFD0MJXiBi1Tl1l_lTBP3mNk" />
-        <meta httpEquiv="REFRESH" content="0;url=https://chaitanyak.info"/>
+        {/* <meta httpEquiv="REFRESH" content="0;url=https://chaitanyak.info"/> */}
         {/* Adding up the Meta Tags For Better SEO Optimizations */}
         <meta property="og:title" content="Chaitanya Khiratkar" />
         <meta property="og:url" content="https://chaitanya-khiratkar.web.app" />
