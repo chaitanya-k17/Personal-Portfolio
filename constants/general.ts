67b40abb2b9d4f8932ff2c2584fd795727f9bbd4
Chaitanya Khiratkar <chaitanya.khiratkar@edfora.com>
2024-12-09 22:33:09 +0530

    Commit

--- constants/general.ts
+++ constants/general.ts
@@ -1,7 +1,7 @@
 export enum RESUME_SUB_TEXT_TYPES {
-    LOCATION = 'location',
-    DATE = 'date',
-    CGPA = 'cgpa',
-    PERCENT = 'percent',
-    LINK = 'link'
+  LOCATION = 'location',
+  DATE = 'date',
+  CGPA = 'cgpa',
+  PERCENT = 'percent',
+  LINK = 'link'
 }
